906cdc2b52a3397cbd1bf735c0f8b97c0d8df7e2
fantom2022 <759153751953@Aa>
2022-11-10 22:50:58 +0300

    rest init

---
+++ lims-rest/src/test/java/com/artg/lims/LimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artg.lims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
